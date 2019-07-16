b15cfc0329d4dce3ff5c05d114d2b4d162f57430
mugunapiero <mugunapiero@gmail.com>
2019-07-16 22:00:41 +0300

    Changed back to using facility county and not countyy

--- reporting/views.py
+++ reporting/views.py
@@ -341,6 +341,30 @@ def get_quarter_report(request, quarter, mwaka):
 	return render(request, template, context)
 
 
+
+
+# Quarterly nets distribution for all counties
+@login_required(login_url='login')
+def quarter_distribution_csv(request, quarter, mwaka):
+	if quarter == "One":
+		quarter_dist = Nets_distributed.objects.filter(date_issued__year__gte=mwaka, date_issued__month__gte=1, date_issued__year__lte=mwaka, date_issued__month__lte=3).values('facility__county').annotate(totalnets=Sum('nets_issued')).order_by('-totalnets')
+	elif quarter == "Two":
+		quarter_dist = Nets_distributed.objects.filter(date_issued__year__gte=mwaka, date_issued__month__gte=4, date_issued__year__lte=mwaka, date_issued__month__lte=6).values('facility__county').annotate(totalnets=Sum('nets_issued')).order_by('-totalnets')
+	elif quarter == "Three":
+		quarter_dist = Nets_distributed.objects.filter(date_issued__year__gte=mwaka, date_issued__month__gte=7, date_issued__year__lte=mwaka, date_issued__month__lte=9).values('facility__county').annotate(totalnets=Sum('nets_issued')).order_by('-totalnets')
+	elif quarter == "Four":
+		quarter_dist = Nets_distributed.objects.filter(date_issued__year__gte=mwaka, date_issued__month__gte=10, date_issued__year__lte=mwaka, date_issued__month__lte=12).values('facility__county').annotate(totalnets=Sum('nets_issued')).order_by('-totalnets')
+
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename="quarterly_distribution.csv"'
+	writer = csv.writer(response)
+	writer.writerow(['County', 'Nets Issued'])
+
+	for report in quarter_dist:
+	    writer.writerow(report)
+
+	return response
+
 @login_required(login_url='login')
 def download_qdistribution_excel(request, quarter, mwaka):
 	today = datetime.datetime.now()
